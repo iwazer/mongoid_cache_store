1caebc51f4e65609ba158a1a3f77f8d0c2a0083b
Eiji Iwazawa <e-iwazawa@mapion.co.jp>
2012-10-18 11:08:51 +0900

    add expires field

--- lib/active_support/cache/mongoid_cache_store.rb
+++ lib/active_support/cache/mongoid_cache_store.rb
@@ -4,6 +4,7 @@ require 'active_support'
 module ActiveSupport
   module Cache
     class MongoidCacheStore < Store
+      DEFAULT_EXPIRES_IN = 86400
 
       class CacheStore
         include Mongoid::Document
@@ -11,6 +12,7 @@ module ActiveSupport
         attr_accessible :_id
 
         field :id, type: String
+        field :expires, type: DateTime, default: -> { Time.now + DEFAULT_EXPIRES_IN }
       end
 
       def initialize options={}
--- spec/mongoid_cache_store_spec.rb
+++ spec/mongoid_cache_store_spec.rb
@@ -35,4 +35,18 @@ describe MongoidCacheStore::CacheStore do
       expect { create }.to raise_error(/duplicate key/)
     end
   end
+
+  describe "expires field" do
+    let(:now_time) { Time.parse('2012-01-01 13:00:00') }
+    before do
+      Time.should_receive(:now).any_number_of_times.and_return(now_time)
+    end
+    context "MongoidCacheStore#new without expires_in option" do
+      let!(:store) { MongoidCacheStore.new }
+      it "should set current time + MongoidCacheStore::DEFAULT_EXPIRES_IN as default" do
+        c = MongoidCacheStore::CacheStore.create(_id: "KEY_STRING")
+        c.reload.expires.should == now_time + MongoidCacheStore::DEFAULT_EXPIRES_IN
+      end
+    end
+  end
 end
